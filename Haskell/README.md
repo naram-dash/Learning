29d823aee884d3f8160fb55ddfb7d3340a225c45
naram.dash <juho_kim@outlook.com>
2021-07-15 16:52:57 +0900

    delete haskell folder

--- Haskell/README.md
+++
@@ -1,5 +0,0 @@
-# Haskell About
-
-These codes are in Learning Haskell For Great Good (Korean).
-
-There are many differences with recent book
--- Haskell/palindromes.hs
+++
@@ -1,7 +0,0 @@
-main = interact respondPalindromes
-
-respondPalindromes :: String -> String
-respondPalindromes =
-    unlines . map (\xs -> if isPal xs then "Palindrome" else "Not a Pal") . lines
-    where   isPal :: String -> Bool
-            isPal xs = xs == reverse xs
