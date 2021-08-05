85975cce7d330a4777fa472212f0624e12beba9e
naram.dash <juho_kim@outlook.com>
2021-08-05 23:01:40 +0900

    fsgu - section 2 complete

--- FSharp From the Ground Up/10_RunningAndDebuggingPrograms/Before/helloword/Program.fs
+++ FSharp From the Ground Up/10_RunningAndDebuggingPrograms/Before/helloword/Program.fs
@@ -4,5 +4,6 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "Hello World from my F# program"
+    printfn "The args are: %A" argv
     0 // return an integer exit code
